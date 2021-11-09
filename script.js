32fb958a3eb7172363dbc43ac54b242b0d7dd1c9
Wazecode <78682268+Wazecode@users.noreply.github.com>
2021-11-09 06:26:45 +0530

    Final Changes to js file

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    a = document.getElementById("rate").value;
+    r = document.getElementById("select").value;
+
+    const currentYear = 2021;
+
+    document.getElementById("result").innerHTML = p*a*r/1000;
+    document.getElementById("principalO").innerHTML = p;
+    document.getElementById("rateO").innerHTML = a/10;
+    document.getElementById("year").innerHTML = (r*10)/10 + (currentYear*10)/10;
     
 }
-        
+
+function show_value(x)
+{
+    var a = document.getElementById("rate");
+    document.getElementById("poop").innerHTML = x/10; 
+}
